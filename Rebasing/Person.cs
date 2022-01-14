238fde69e411ceff763aaa945c8dad3c44716d2d
Karionis <niklas.kanthak@online.de>
2022-01-14 18:49:52 +0100

    Added Weight property

--- Rebasing/Person.cs
+++ Rebasing/Person.cs
@@ -11,5 +11,6 @@ namespace Rebasing
         public double Age { get; set; } = 35;
         public string Name { get; set; }
         public double Height { get; set; }
+        public double Weight { get; set; }
     }
 }
